e3b2716b37c6a2a5aa06d16b598ee64ff48e8d5c
Probir Sarkar <81101648+probir-sarkar@users.noreply.github.com>
2021-12-01 14:09:59 +0530

    s

--- api/defines.php
+++ api/defines.php
@@ -1,5 +1,4 @@
 <?php
 define('INSTAGRAM_APP_ID', '423043692653490');
 define('INSTAGRAM_APP_SECRET', '2d9368ebf27e72a8109b4e4f175ad87a');
-define('INSTAGRAM_APP_REDIRECT_URI', 'https: //champoffroad.herokuapp.com/api/
-');
+define('INSTAGRAM_APP_REDIRECT_URI', 'https:champoffroad.herokuapp.com/api/');
